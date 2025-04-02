ca844a4060d259f4963a173f7b8bb3490c7ef1ce
petar101 <53666612+petar101@users.noreply.github.com>
2025-04-02 18:30:50 +1100

    did first 3 tests

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,57 @@
 module Enumerable
-  # Your code goes here
+
+  def my_all?
+    # Start by assuming everything matches (true)
+    result = true
+    
+    # Iterate through each element using my_each
+    self.my_each do |item|
+      # Yield the item to the block and check its result
+      unless yield(item)
+        result = false
+        break  # Exit early if we find any false result
+      end
+    end
+    result
+  end
+
+  def my_any? 
+    result = false
+
+    self.my_each do |item|
+      if yield(item)
+        result = true 
+        break
+      end
+    end
+    result
+  end
+
+  def my_count 
+    count = 0
+  
+    self.my_each do |item|
+      if block_given?
+        count += 1
+      else 
+        count += 1
+      end
+    end
+  
+    count 
+  end
+  
+   
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  
+  def my_each
+    for item in self ## every item in the object thats calling it
+      ## self is the object that this method is being parsed through. 
+      yield(item) ## give that statement to the block to do something with it
+    end
+    self #because we are altering the array, we need to call it back to the array. 
+  end
+  
 end
